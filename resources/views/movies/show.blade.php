006a5e5739733e9024921c112b9aabfa02ed1d5a
clive0417 <clive0417@gmail.com>
2020-09-29 22:11:20 +0800

    add release date poster overview information

---
+++ resources/views/movies/show.blade.php
@@ -0,0 +1,34 @@
+<!--layouts. 的 . 代表資料夾-->
+@extends('layouts.app')
+
+@section('page_title')
+<section class="page-title">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h4 class="text-uppercase">Single Movie Detail info</h4>
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="{{ action('HomeController@index') }}">Home</a>
+                            </li>
+                            <li class="breadcrumb-item"><a href="{{ action('MovieController@index') }}">Movie Admin table</a>
+                            </li>
+                            <li class="breadcrumb-item active">Single Movie Detail info</li>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+</section>
+@endsection
+
+@section('content')
+<div class="page-content">
+    <div class="container">
+        {{-- 是因為update 時要傳舊資料 --}}
+    @include('movies.movieform',['movie'=>$movie],['isCreate'=> $isCreate])
+
+    </div>
+</div>
+
+
+@endsection
+            
