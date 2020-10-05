07ac3f99a079bff7ac6b549454fd20a82c8f68c8
clive0417 <clive0417@gmail.com>
2020-10-05 13:56:16 +0800

    modify movies soft deletes

--- app/Movie.php
+++ app/Movie.php
@@ -2,11 +2,15 @@
 
 namespace App;
 
+
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Movie extends Model
 {
     //
+    use SoftDeletes;
+    protected $dates = ['deleted_at'];
     protected $fillable = ['title','TMDB_id','posterUrl','releaseDate','point','price','overview'];
     public function genres()
     {
